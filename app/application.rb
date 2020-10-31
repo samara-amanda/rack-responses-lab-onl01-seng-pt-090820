5a91354e9dff1038a47421d84ca3c5fc786a5e13
Samara Grant <samara.amanda01@gmail.com>
2020-10-31 05:30:11 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,10 +4,10 @@ class Application
 
     time = Kernel.rand(1..24)
 
-    if time >= 12
-      resp.write "Good Afternoon!"
-    else
+    if time <= 12
       resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
     end
     resp.finish
   end
