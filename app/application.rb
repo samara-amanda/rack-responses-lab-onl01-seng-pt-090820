9af4a455117d2a41f18962ee0aeeb56d378dd067
Samara Grant <samara.amanda01@gmail.com>
2020-10-31 05:22:26 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,12 @@
+class Applicaiton
+  def call(env)
+    resp = Rack::Response.new
+
+    time = Kernal.rand(1..24)
+
+    if time >= 12
+      resp.write "Good Afternoon!"
+    else
+      resp.write "Good Morning!"
+  end
+end
